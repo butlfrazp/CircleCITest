7b1929754fbd23415fc9815561232a061fa93ed3
dillon-ms <dillonhall@microsoft.com>
2022-10-04 18:04:04 -0500

    Add test function project

---
+++ CircleCiTestFunction/CircleCiTestFunctionTests/CircleCiTestFunctionUnitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CircleCiTestFunctionTests;
+
+public class CircleCiTestFunctionUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ThisTestWillAlwaysFail()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
+    public void ThisTestWillAlwyasPass()
+    {
+        Assert.Pass();
+    }
+}
