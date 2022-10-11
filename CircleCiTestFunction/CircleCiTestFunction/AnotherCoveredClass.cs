8ed1eabe59c2b534f0e586b6c96187a04399a169
dillon-ms <dillonhall@microsoft.com>
2022-10-11 09:32:43 -0500

    Add second unit test class + threshold param

---
+++ CircleCiTestFunction/CircleCiTestFunction/AnotherCoveredClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CircleCiTestFunction
+{
+    public class AnotherCoveredClass
+    {
+        public AnotherCoveredClass()
+        {
+        }
+
+        public float AddNumbers(float x, float y) => x + y;
+
+        public float DivideNumbers(float x, float y)
+        {
+            if (y == 0) return 0;
+
+            return x / y;
+        }
+
+        public string GetString(int x)
+        {
+            if (x < 10) return "Less than 10";
+
+            if (x < 50) return "Less than 50";
+
+            if (x < 100) return "Less than 100";
+
+            return "Greater than 100";
+        }
+    }
+}
+
